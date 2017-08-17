04922c5e935630fec0d68ce429a39c913823bc36
Michael E. Chancey Jr <michael.chancey@gmail.com>
2017-08-16 20:50:39 -0400

    Lesson 1 Fibonacci Sequence

---
+++ index.js
@@ -0,0 +1,27 @@
+function calculateFibonacci(sequenceLength) {
+  // DECLARE AN ARRAY FOR HOLDING VALUES
+  // CREATE A LOOP TO CALCULATE THE FIBONACCI SEQUENCE
+  // RETURN THE ARRAY THAT SHOULD NOW HOLD THE FIBONACCI SEQUENCE
+}
+
+function printArray(arrayToPrint) {
+  // CREATE A LOOP TO DISPLAY THE CONTENTS OF arrayToPrint
+}
+
+var sequence = calculateFibonacci(10);
+printArray(sequence);
+
+// EXPECTED OUTPUT:
+// 0
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
+// 34
+
+// BONUS POINTS IF YOU GET: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+// HINT: array.join()
