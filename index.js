c9aa4a88183c23fa0990ccd88576f75b0fa06ff9
nhan <nhngt87@gmail.com>
2017-09-24 21:47:00 -0400

    ToDo App

--- index.js
+++ index.js
@@ -3,26 +3,44 @@ const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 //TODO: IMPORT TODO DATABASE ACCESS
+const todoDb = require('./utils/dataStore');
 
 //SETUP HANDLEBARS VIEW ENGINE
 app.engine('handlebars', hbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //TODO: SETUP MIDDLE WARE AND ALSO THE STATIC PAGE ROUTE
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //TODO: SETUP ROUTE FOR INCOMPLETE TASKS
-app.get('/', (request, response) => {});
+app.get('/', (request, response) => {
+    const incompleteTodos = todoDb.getIncompleteTodos();
+response.render('todos', {home: true, incompleteTodos});
+});
 
 //TODO: SETUP ROUTE FOR COMPLETED TASKS
-app.get('/completed', (request, response) => {});
+app.get('/completed', (request, response) => {
+    const pageData = {
+        completed: true,
+        completeTodos: todoDb.getCompletedTodos()
+    };
+response.render('completed', pageData);
+});
 
 //TODO: SETUP POSTING OF A NEW TODO
-app.post('/todo', (request, response) => {});
+app.post('/todo', (request, response) => {
+    const { body } = request;
+todoDb.addTodo(body.newTodo);
+response.redirect('/');
+});
 
 //TODO: SETUP THE COMPLETING OF A TASK
 app.post('/complete', (request, response) => {});
 
 //TODO: SETUP ROUTE FOR ALL PAGES A USER COULD TYPE
-app.get('/*', (request, response) => {});
+app.get('/*', (request, response) => {
+    response.redirect('/');
+});
 
 app.listen(3000);
--- utils/dataStore.js
+++ utils/dataStore.js
@@ -2,19 +2,45 @@ const fs = require('fs');
 const dataFilePath = './dataStore/data.json';
 
 //TODO: GET ALL TODOS FROM DISK
-function getAllTodos() {}
+function getAllTodos() {
+    return JSON.parse(fs.readFileSync(dataFilePath));
+}
 
 //TODO: GET COMPLETED LIST OF TODOS
-function getCompletedTodos() {}
+function getCompletedTodos() {
+    return getAllTodos().filter(todo => todo.completed);
+}
 
 //TODO: GET INCOMPLETE TODO LIST
-function getIncompleteTodos() {}
+function getIncompleteTodos() {
+    return getAllTodos().filter((todo) => {
+        return !todo.completed;
+});
+}
 
 //TODO: ADD A NEW TODO TO THE LSIT
-function addTodo(todo) {}
+function addTodo(todo) {
+    const currentTodos = getAllTodos();
+    currentTodos.push({
+        completed: false,
+        task: todo,
+        id: new Date().getTime(),
+    })
+
+    fs.writeFileSync(dataFilePath, JSON.stringify(currentTodos));
+}
 
 //TODO: FIND AND COMPLETE A TODO BASED OFF OF THE ID
-function completeTodo(id) {}
+function completeTodo(id) {
+    //GET ALL TODOS
+    //FIND TODO BY ID HINT: todoList.findIndex(todo => todo.id === id)
+    //MODIFY TODO BY SETTING COMPLETED = TRUE HINT: todoList(index).completed = false;
+    //SAVE LIST BACK TO DISK
+}
 
 //TODO: EXPORT ALL METHODS FOR ACCESS THROUGHOUT THE APP
-module.exports = {};
+module.exports = {
+    getIncompleteTodos,
+    getCompletedTodos,
+    addTodo
+};
